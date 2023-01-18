6a1e6de690c30e8d2fa7d98988a858980ff5341d
Ellie <literallyEllie@users.noreply.github.com>
2023-01-18 18:25:44 +0000

    remove  --recursive flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ fi
 mc alias set ${insecure_option:+"$insecure_option"} deploy "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
 
 if [[ "$MINIO_CP" == "true" ]]; then
-  mc cp --recursive ${insecure_option:+"$insecure_option"} $1 "deploy/$2"
+  mc cp ${insecure_option:+"$insecure_option"} $1 "deploy/$2"
 else
   mc mirror --overwrite ${insecure_option:+"$insecure_option"} $1 "deploy/$2"
 fi
